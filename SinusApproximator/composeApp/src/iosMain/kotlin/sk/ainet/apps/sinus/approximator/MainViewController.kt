bbf547081d0fa5bbe75dde5940b28a0c9b40b03c
Michal Harakal <michal.harakal@googlemail.com>
2025-11-08 14:51:08 +0100

    Migrate iOS App to Compose Multiplatform

---
+++ SinusApproximator/composeApp/src/iosMain/kotlin/sk/ainet/apps/sinus/approximator/MainViewController.kt
@@ -0,0 +1,5 @@
+package sk.ainet.apps.sinus.approximator
+import androidx.compose.ui.window.ComposeUIViewController
+import sk.ai.net.samples.kmp.sinus.approximator.App
+
+fun MainViewController() = ComposeUIViewController { App() }
